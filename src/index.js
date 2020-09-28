037f0695510a4e25ae3234fe0f9e9ac0b1204bf0
info@kawoolutions.com <info@kawoolutions.com>
2020-09-28 20:31:37 +0200

    Resolves #3

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import ReactDOM from 'react-dom';
 import TestManager from './TestManager';
 
 import './index.css';
-// import './styles/borders.css';
-// import './styles/colors.css';
-// import './styles/fonts.css';
-// import './styles/forms.css';
-// import './styles/heights.css';
-// import './styles/margins.css';
-// import './styles/misc.css';
-// import './styles/paddings.css';
-// import './styles/text.css';
-// import './styles/widths.css';
+import './styles/borders.css';
+import './styles/colors.css';
+import './styles/fonts.css';
+import './styles/heights.css';
+import './styles/margins.css';
+import './styles/misc.css';
+import './styles/paddings.css';
+import './styles/primeflex.css';
+import './styles/text.css';
+import './styles/widths.css';
 
 // ReactDOM.render(<PersonManager />, document.getElementById('root'));
 ReactDOM.render(<TestManager />, document.getElementById('root'));
