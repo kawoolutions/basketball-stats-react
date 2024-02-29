c89593eb2c212a851165b297bd492c9c4527ce9a
kʰavu'luːʃənz <info@kawoolutions.com>
2024-02-29 23:57:06 +0100

    Created components dir and moved PersonManager.js there

--- src/components/PersonManager.js
+++ src/components/PersonManager.js
@@ -18,9 +18,9 @@ import 'primereact/resources/themes/luna-blue/theme.css';
 import 'primereact/resources/primereact.css';
 import 'primeicons/primeicons.css';
 
-import './manager.css';
+import '../manager.css';
 
-const packageJson = require('../package.json');
+const packageJson = require('../../package.json');
 
 // console.log("PJ\n" + JSON.stringify(packageJson));
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 
-import PersonManager from './PersonManager';
+import PersonManager from './components/PersonManager';
 // import TestManager from './TestManager';
 
 import './index.css';
