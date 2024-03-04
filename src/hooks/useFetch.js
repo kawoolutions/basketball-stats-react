d3d94ba6b407e27b4834041e1b0cf1df11116660
kʰavu'luːʃənz <info@kawoolutions.com>
2024-03-04 13:01:27 +0100

    Added useFetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+export default function useFetch(url) {
+    console.log("useFetch");
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        console.log("useEffect");
+        async function init() {
+            try {
+                const response = await fetch(url);
+                if (response.ok) {
+                    const json = await response.json();
+                    setData(json);
+                } else {
+                    throw response;
+                }
+            } catch (e) {
+                setError(e);
+            } finally {
+                setLoading(false);
+            }
+        }
+        init();
+    }, [url]);
+
+    return { data, loading, error };
+}
