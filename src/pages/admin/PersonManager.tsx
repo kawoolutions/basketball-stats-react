82b2990880f96e0a38d981096a5d3d32a57914d1
kʰavu'luːʃənz <info@kawoolutions.io>
2024-04-07 22:58:17 +0200

    Implemeted roles for person manager

--- src/pages/admin/PersonManager.tsx
+++ src/pages/admin/PersonManager.tsx
@@ -61,6 +61,27 @@ export default function PersonManager() {
         );
     };
 
+    const rolesBody = (person: { player: object, coach: object, referee: object }) => {
+        const nodes: Array<ReactNode> = [];
+
+        if (person.player) {
+            nodes.push(<span title={t("entity.player.singular.label")} style={{width: "20px"}}>⛹️</span>);
+        }
+        if (person.coach) {
+            nodes.push(<span title={t("entity.coach.singular.label")} style={{width: "20px"}}>👨‍💼</span>);
+        }
+        if (person.referee) {
+            nodes.push(<img src="/src/assets/referee-border.png"
+                title={t("entity.referee.singular.label")}
+                width={15}
+                height={15}
+                alt={t("entity.referee.singular.label")}
+                style={{verticalAlign: "-1px"}}/>);
+        }
+
+        return nodes;
+    };
+
     const streetBody = (person: { streetName: string, houseNbr: string }) => {
         const streetName = person.streetName;
         const houseNumber = person.houseNbr;
@@ -94,7 +115,7 @@ export default function PersonManager() {
         return person.birthDate ? dateFormat.format(date) : null;
     }
 
-    const emailAddresses = (person: { emailAddresses: [] }) => {
+    const emailAddressesBody = (person: { emailAddresses: [] }) => {
 //        return person.emailAddresses.join(", ");
 
         return (
@@ -117,7 +138,7 @@ export default function PersonManager() {
         subscriberNbr: number;
     }
 
-    const mobilePhoneNumber = (person: { phoneNumbers: Record<string, PhoneNumber> }) => {
+    const phoneNumbersBody = (person: { phoneNumbers: Record<string, PhoneNumber> }) => {
         const phoneNumbers = person.phoneNumbers;
 //        for (const [key, phoneNumber] of Object.entries(phoneNumbers)) {
 //            console.log(key, "+" + phoneNumber!.countryCode + " " + phoneNumber!.areaCode + " " + phoneNumber!.subscriberNbr);
@@ -155,14 +176,14 @@ export default function PersonManager() {
                 <Column field="lastName" header={t("common.lastName.heading")} sortable style={{width: "9%"}}/>
                 <Column field="firstName" header={t("common.firstName.heading")} sortable style={{width: "9%"}}/>
                 <Column field="incognito" header={t("entity.person.incognito.heading")} body={incognitoBody} sortable style={{width: "6.5%"}}/>
-                <Column field="roles" header={t("common.role.plural.heading")} style={{width: "5.5%"}}/>
+                <Column field="roles" header={t("common.role.plural.heading")} body={rolesBody} style={{width: "5.5%"}}/>
                 <Column field="streetName" header={t("common.street.heading")} body={streetBody} sortable style={{width: "8%"}}/>
                 <Column field="zipCode" header={t("common.zipCode.heading")} sortable style={{width: "5.5%"}}/>
                 <Column field="cityName" header={t("common.city.heading")} sortable style={{width: "7%"}}/>
                 <Column field="countryCode" header={t("common.country.heading")} body={countryBody} sortable style={{width: "7.5%"}}/>
                 <Column field="birthDate" header={t("common.birthDate.heading")} body={birthDateBody} sortable style={{width: "10%"}}/>
-                <Column field="emailAddresses" header={t("common.emailAddress.heading")} body={emailAddresses} sortable style={{width: "10.5%"}}/>
-                <Column field="phoneNumbers" header={t("entity.phoneNumber.plural.heading")} body={mobilePhoneNumber} sortable style={{width: "9%"}}/>
+                <Column field="emailAddresses" header={t("common.emailAddress.heading")} body={emailAddressesBody} sortable style={{width: "10.5%"}}/>
+                <Column field="phoneNumbers" header={t("entity.phoneNumber.plural.heading")} body={phoneNumbersBody} sortable style={{width: "9%"}}/>
                 <Column field="actions" style={{width: "7%"}}/>
             </DataTable>
         </div>
