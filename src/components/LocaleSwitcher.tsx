6062ab9c8d4dde9ccb30032b18fa41af9b29304c
kʰavu'luːʃənz <info@kawoolutions.com>
2024-04-18 14:48:38 +0200

    Removed spaces

--- src/components/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher.tsx
@@ -1,5 +1,5 @@
-import { useTranslation } from "react-i18next";
-import { supportedLngs } from "../i18n/config.ts";
+import {useTranslation} from "react-i18next";
+import {supportedLngs} from "../i18n/config.ts";
 
 import {Dropdown} from "primereact/dropdown";
 
